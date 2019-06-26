d9283e231219f6c1aed1ab4b4f7a2d9b2adf2bca
Max Goedjen <max.goedjen@gmail.com>
2019-06-26 00:22:16 -0700

    Sample data

--- Calendarish WatchKit Extension/ContentView.swift
+++ Calendarish WatchKit Extension/ContentView.swift
@@ -4,18 +4,32 @@ import CalendarishCoreWatch
 struct ContentView : View {
 
     @State var store: Store
+    let dateFormatter: DateFormatter = {
+        let f = DateFormatter()
+        f.timeStyle = .short
+        return f
+    }()
+
+    let durationFormatter: DateComponentsFormatter = {
+        let f = DateComponentsFormatter()
+        f.allowedUnits = [.hour, .minute]
+        return f
+    }()
 
     var body: some View {
-        VStack {
-            List(store.events) { event in
-                VStack(alignment: .leading) {
-                    Text(event.name)
-                        .font(.subheadline)
-                        .fontWeight(.bold)
-                    Text(DateFormatter().string(from: event.startTime))
+        List(store.events) { event in
+            VStack(alignment: .leading) {
+                Text(event.name)
+                    .font(.headline)
+                    .fontWeight(.bold)
+                HStack {
+                    Text(self.dateFormatter.string(from: event.startTime))
+                    Spacer()
+                Text(self.durationFormatter.string(from: event.startTime, to: event.endTime)!)
                 }
+                }
+                .padding()
             }.listStyle(.carousel)
-        }
     }
 
 }
@@ -24,14 +38,45 @@ struct ContentView : View {
 struct ContentView_Previews : PreviewProvider {
     static var previews: some View {
         ContentView(store: Store(events: [
-            Event(identifier: UUID().uuidString, name: "Coffee with Marina", startTime: Date(), endTime: Date(), calendar: CalendarishCoreWatch.Calendar(identifier: UUID().uuidString, name: "Home")),
-            Event(identifier: UUID().uuidString, name: "iOS Sync", startTime: Date(), endTime: Date(), calendar: CalendarishCoreWatch.Calendar(identifier: UUID().uuidString, name: "Work")),
-            Event(identifier: UUID().uuidString, name: "Team Lunch", startTime: Date(), endTime: Date(), calendar: CalendarishCoreWatch.Calendar(identifier: UUID().uuidString, name: "Work")),
-            Event(identifier: UUID().uuidString, name: "Coffee with Marina", startTime: Date(), endTime: Date(), calendar: CalendarishCoreWatch.Calendar(identifier: UUID().uuidString, name: "Work")),
-            Event(identifier: UUID().uuidString, name: "Dinner with Parents", startTime: Date(), endTime: Date(), calendar: CalendarishCoreWatch.Calendar(identifier: UUID().uuidString, name: "Home")),
+            Event(identifier: UUID().uuidString,
+                  name: "Coffee with Marina",
+                  startTime: Date(timeIntervalSince1970: -25200),
+                  endTime:  Date(timeIntervalSince1970: -23400),
+                  attendees: [],
+                  description: nil,
+                  location: "Menotti's",
+                  calendar: CalendarishCoreWatch.Calendar(identifier: UUID().uuidString, name: "Home")),
+            Event(identifier: UUID().uuidString,
+                  name: "iOS Sync",
+                  startTime: Date(timeIntervalSince1970: -21600),
+                  endTime: Date(timeIntervalSince1970: -19800),
+                  attendees: [],
+                  description: "Catch up on projects.",
+                  location: "Blue Devils",
+                  calendar: CalendarishCoreWatch.Calendar(identifier: UUID().uuidString, name: "Work")),
+            Event(identifier: UUID().uuidString,
+                  name: "Team Lunch",
+                  startTime: Date(timeIntervalSince1970: -14400),
+                  endTime: Date(timeIntervalSince1970: -10800),
+                  attendees: [],
+                  description: nil,
+                  location: "Blossom",
+                  calendar: CalendarishCoreWatch.Calendar(identifier: UUID().uuidString, name: "Work")),
+            Event(identifier: UUID().uuidString,
+                  name: "Dinner with Parents",
+                  startTime: Date(timeIntervalSince1970: 7200),
+                  endTime: Date(timeIntervalSince1970: 10800),
+                  attendees: [],
+                  description: nil,
+                  location: "Blossom",
+                  calendar: CalendarishCoreWatch.Calendar(identifier: UUID().uuidString, name: "Home")),
+
             ]))
     }
 }
 #endif
+
+
+
 
 
---
+++ CalendarishCore/Attendee.swift
@@ -0,0 +1,9 @@
+//
+//  Attendee.swift
+//  CalendarishCore
+//
+//  Created by Max Goedjen on 6/26/19.
+//  Copyright © 2019 Max Goedjen. All rights reserved.
+//
+
+import Foundation
