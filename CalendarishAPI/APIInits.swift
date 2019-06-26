b6b90e4226f32bffc1dce0bb23f40db570289880
Max Goedjen <max.goedjen@gmail.com>
2019-06-26 01:22:10 -0700

    Parse

--- CalendarishAPI/APIInits.swift
+++ CalendarishAPI/APIInits.swift
@@ -14,7 +14,25 @@ extension CalendarishCore.Calendar {
 extension CalendarishCore.Event {
 
     init(_ event: GTLRCalendar_Event, calendar: CalendarishCore.Calendar) {
-        self.init(identifier:  event.identifier!, name: event.summary!, startTime: event.start?.dateTime?.date ?? Date(), endTime: event.end?.dateTime?.date ?? Date(), attendees: [], description: event.description, location: event.location, calendar: calendar)
+        self.init(identifier:  event.identifier!,
+                  name: event.summary!,
+                  startTime: event.start?.dateTime?.date ?? Date(),
+                  endTime: event.end?.dateTime?.date ?? Date(),
+                  attendees: (event.attendees ?? []).map({ Attendee($0) }),
+                  description: event.description,
+                  location: event.location,
+                  calendar: calendar)
+    }
+
+}
+
+extension CalendarishCore.Attendee {
+
+    init(_ attendee: GTLRCalendar_EventAttendee) {
+        self.init(identifier: attendee.identifier!,
+                  name: attendee.displayName!,
+                  response: Response(rawValue: attendee.responseStatus!)!
+                  )
     }
 
 }
