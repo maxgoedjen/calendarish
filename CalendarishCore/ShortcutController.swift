94fe7f11cacec8966a1b7b1ab04dca3a8c2fee48
Max Goedjen <max.goedjen@gmail.com>
2019-06-29 15:06:47 -0700

    Shortcut generation

--- CalendarishCore/ShortcutController.swift
+++ CalendarishCore/ShortcutController.swift
@@ -3,6 +3,12 @@ import Intents
 
 class ShortcutController {
 
+    var dateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.timeStyle = .short
+        return formatter
+    }
+
     var events: [Event] = [] {
         didSet {
             updateIntents()
@@ -15,25 +21,31 @@ extension ShortcutController {
 
     func updateIntents() {
         let shortcuts = events.map({ shortcut(for: $0 )})
-        print(shortcuts)
+        // TODO: REMOVE
+        let test = Array(shortcuts[0...1])
+        INRelevantShortcutStore.default.setRelevantShortcuts(test, completionHandler: nil)
     }
 
-    func shortcut(for event: Event) -> INShortcut {
-        fatalError()
-//        let intent = INIntent()
-//        intent.identifier = event.identifier
-//        intent.intentDescription = event.description
-//        guard let shortcut = INShortcut(intent: intent) else { return nil }
-//
-//        let suggestedShortcut = INRelevantShortcut(shortcut: shortcut)
-//
-//        let localizedTitle = NSString.deferredLocalizedIntentsString(with: event.name) as String
-//        let template = INDefaultCardTemplate(title: localizedTitle)
-//        template.subtitle =
-////        template.image = INImage(syste)
-//
-//        suggestedShortcut.watchTemplate = template
-//        return suggestedShortcut
+    func shortcut(for event: Event) -> INRelevantShortcut {
+        let activity = NSUserActivity(activityType: "com.calendarish.event")
+        activity.title = event.name
+        activity.expirationDate = event.endTime
+
+        let base = INShortcut(userActivity: activity)
+        let shortcut = INRelevantShortcut(shortcut: base)
+        let card = INDefaultCardTemplate(title: event.name)
+        let startString = dateFormatter.string(from: event.startTime)
+        let subtitle: String
+        if let location = event.location {
+            subtitle = "\(startString) \(location)"
+        } else {
+            subtitle = startString
+        }
+        card.subtitle = subtitle
+        shortcut.shortcutRole = .information
+        shortcut.relevanceProviders = [INDateRelevanceProvider(start: event.startTime, end: event.endTime)]
+        shortcut.watchTemplate = card
+        return shortcut
     }
 
 }
