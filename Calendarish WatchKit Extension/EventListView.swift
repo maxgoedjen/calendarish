0be8e3426730749892e450998378e03bca8aae71
Max Goedjen <max.goedjen@gmail.com>
2019-10-10 22:18:03 -0700

    Fix view

--- Calendarish WatchKit Extension/EventListView.swift
+++ Calendarish WatchKit Extension/EventListView.swift
@@ -41,8 +41,7 @@ struct EventListView: View {
                     }
                     .padding()
                 }
-        }
-            .listStyle(.carousel)
+        }.listStyle(CarouselListStyle())
     }
 
 }
