778071d1d3bcc328b61751555bc36c21f563b25d
Max Goedjen <max.goedjen@gmail.com>
2021-01-30 22:35:00 -0800

    Fix merging many

--- CalendarishAPI/BatchAPI.swift
+++ CalendarishAPI/BatchAPI.swift
@@ -15,7 +15,7 @@ public class BatchAPI {
 extension BatchAPI: APIProtocol {
 
     public var eventPublisher: AnyPublisher<[Event], API.Error> {
-        return Publishers.MergeMany(apis.map({ $0.eventPublisher }))
+        return CombineLatestArray(apis.map({ $0.eventPublisher }))
             .map { $0.sorted() }
             .removeDuplicates()
             .eraseToAnyPublisher()
@@ -28,3 +28,31 @@ extension BatchAPI: APIProtocol {
     }
 
 }
+
+private struct CombineLatestArray<PublisherType, T> : Publisher where PublisherType : Publisher, PublisherType.Output == [T] {
+
+    typealias Output = (PublisherType.Output)
+    typealias Failure = (PublisherType.Failure)
+
+    private let combined: AnyPublisher<PublisherType.Output, PublisherType.Failure>
+
+    init(_ publishers: [PublisherType]) {
+        if publishers.isEmpty {
+            combined = PassthroughSubject<PublisherType.Output, PublisherType.Failure>().eraseToAnyPublisher()
+        } else if publishers.count == 1 {
+            combined = publishers.first!.eraseToAnyPublisher()
+        } else {
+            var running = publishers.first!.eraseToAnyPublisher()
+            for publisher in publishers[1...] {
+                running = running.combineLatest(publisher, +).eraseToAnyPublisher()
+            }
+            combined = running
+        }
+    }
+
+    func receive<S>(subscriber: S) where S : Subscriber, Self.Failure == S.Failure, Self.Output == S.Input {
+        combined.receive(subscriber: subscriber)
+    }
+
+}
+
